1aab5b1e8cb45b41fcea62ba510768f6fa0b16e5
Kevin Zhang <kwz5@pitt.edu>
2017-03-28 01:06:24 -0400

    search working perfectly

--- site/app/mapreduce/search.java
+++ site/app/mapreduce/search.java
@@ -26,6 +26,11 @@ public class search {
     private static PrintWriter resultWriter;
     private static Lemmatizer lem;
 
+    //maps keywords to how many chunks contained the keyword
+    private static HashMap<String, Integer> chunkFreq = new HashMap<>();
+    //maps docName/chunk to corresponding DocData object
+    static HashMap<String, DocData> counts = new HashMap<>();
+
     public static void main(String[] args) throws Exception {
         /* args[0] is input path, args[1] is output path */
         String inputDir = args[0];
@@ -63,6 +68,32 @@ public class search {
         /* run job 1 */
         JobClient.runJob(job1);
 
+        //get set of books that contain keywords and populate docList
+        HashSet<String> relevantDocs = new HashSet<>();
+        DocData [] docList = new DocData[counts.size()];
+        Iterator docDataIt = counts.entrySet().iterator();
+        int index = 0;
+        while(docDataIt.hasNext()){
+            Map.Entry pair = (Map.Entry) docDataIt.next();
+            docList[index] = (DocData) pair.getValue();
+            index++;
+            String documentChunk = (String) pair.getKey();
+            relevantDocs.add(documentChunk.split("/")[0]);
+        }
+
+        resultWriter.println("Relevant Documents");
+        Iterator relDocsIt = relevantDocs.iterator();
+        while(relDocsIt.hasNext()) {
+            resultWriter.println(relDocsIt.next());
+        }
+        for(DocData d : docList){
+            d.computeWeight();
+        }
+        Arrays.sort(docList);
+        for (int i = 0; i < 5; i++) {
+            printChunk(docList[i]);
+        }
+
         resultWriter.flush();
         resultWriter.close();
     }
@@ -70,10 +101,7 @@ public class search {
     public static class TGSearchMapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
 
         public TGSearchMapper() {}
-        //maps keywords to how many chunks contained the keyword
-        private HashMap<String, Integer> chunkFreq = new HashMap<>();
-        //maps docName/chunk to corresponding DocData object
-        HashMap<String, DocData> counts = new HashMap<>();
+
 
 
         public void map(LongWritable key, Text value, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
@@ -112,121 +140,96 @@ public class search {
                 else 
                     chunkFreq.put(term, chunkFreq.get(term)+chunkCount);
 
-                //get set of books that contain keywords and populate docList
-                HashSet<String> relevantDocs = new HashSet<>();
-                DocData [] docList = new DocData[counts.size()];
-                Iterator docDataIt = counts.entrySet().iterator();
-                int index = 0;
-                while(docDataIt.hasNext()){
-                    Map.Entry pair = (Map.Entry) docDataIt.next();
-                    docList[index] = (DocData) pair.getValue();
-                    index++;
-                    String documentChunk = (String) pair.getKey();
-                    relevantDocs.add(documentChunk.split("/")[0]);
-                }
 
-                resultWriter.println("Relevant Documents");
-                Iterator relDocsIt = relevantDocs.iterator();
-                while(relDocsIt.hasNext()) {
-                    resultWriter.println(relDocsIt.next());
-                }
-                for(DocData d : docList){
-                    d.computeWeight();
-                }
-                Arrays.sort(docList);
-                for (int i = 0; i < 5; i++) {
-                    printChunk(docList[i]);
-                }
             }
         }
+    }
+    private static void insertChunkData(String book, int chunk, String term, int count){
+        if(!counts.containsKey(book + "/" + String.valueOf(chunk)))
+            counts.put(book + "/" + String.valueOf(chunk), new DocData(book, chunk));
 
-        private void insertChunkData(String book, int chunk, String term, int count){
-            if(!counts.containsKey(book + "/" + String.valueOf(chunk)))
-                counts.put(book + "/" + String.valueOf(chunk), new DocData(book, chunk));
+        DocData d = counts.get(book + "/" + String.valueOf(chunk));
+        if(!d.termFreq.containsKey(term))
+            d.termFreq.put(term, 0);
+        d.termFreq.put(term,d.termFreq.get(term)+count);
+    }
 
-            DocData d = counts.get(book + "/" + String.valueOf(chunk));
-            if(!d.termFreq.containsKey(term))
-                d.termFreq.put(term, 0);
-            d.termFreq.put(term,d.termFreq.get(term)+count);
+    private static void printChunk(DocData d) throws FileNotFoundException, IOException{
+        File doc = new File("/tmp/tiny_google_input/"+d.docName);
+        byte[] context = new byte[CHUNK_SIZE];
+        RandomAccessFile file = new RandomAccessFile(doc, "r");
+        file.seek(d.chunk*CHUNK_SIZE);
+        file.readFully(context);
+        resultWriter.println(d.docName + " " + d.chunk + " " + String.valueOf(d.weight));
+        resultWriter.println(new String(context));
+
+        Iterator termIt = d.termFreq.entrySet().iterator();
+        while(termIt.hasNext()){
+            Map.Entry pair = (Map.Entry)termIt.next();
+            String term = (String)pair.getKey();
+            int freq = (int)pair.getValue();
+            resultWriter.println(term + " " + String.valueOf(freq));
         }
 
-        private void printChunk(DocData d) throws FileNotFoundException, IOException{
-            File doc = new File("/tmp/tiny_google_input/"+d.docName);
-            byte[] context = new byte[CHUNK_SIZE];
-            RandomAccessFile file = new RandomAccessFile(doc, "r");
-            file.seek(d.chunk*CHUNK_SIZE);
-            file.readFully(context);
-            resultWriter.println(d.docName + " " + d.chunk + " " + String.valueOf(d.weight));
-            resultWriter.println(new String(context));
+    }
 
-            Iterator termIt = d.termFreq.entrySet().iterator();
+    private static class DocData implements Comparable<DocData> {
+        String docName;
+        int chunk;
+            //stores frequencies of keywords
+        HashMap<String, Integer> termFreq;
+        float weight;
+
+        public DocData(String name, int chunk) {
+            this.docName = new String(name);
+            this.chunk = chunk;
+            this.termFreq = new HashMap<>();
+        }
+
+        public void computeWeight(){
+            weight = 0;
+            Iterator termIt = termFreq.entrySet().iterator();
             while(termIt.hasNext()){
                 Map.Entry pair = (Map.Entry)termIt.next();
                 String term = (String)pair.getKey();
                 int freq = (int)pair.getValue();
-                resultWriter.println(term + " " + String.valueOf(freq));
-            }
-
-        }
-
-        private class DocData implements Comparable<DocData> {
-            String docName;
-            int chunk;
-            //stores frequencies of keywords
-            HashMap<String, Integer> termFreq;
-            float weight;
-
-            public DocData(String name, int chunk) {
-                this.docName = new String(name);
-                this.chunk = chunk;
-                this.termFreq = new HashMap<>();
-            }
-
-            public void computeWeight(){
-                weight = 0;
-                Iterator termIt = termFreq.entrySet().iterator();
-                while(termIt.hasNext()){
-                    Map.Entry pair = (Map.Entry)termIt.next();
-                    String term = (String)pair.getKey();
-                    int freq = (int)pair.getValue();
-                    int chunks = chunkFreq.get(term);
-                    //float frac = NUMBER_OF_CHUNKS/chunks;
-                    weight += (freq);
-                }
-            }
-
-            public int compareTo(DocData d) {
-                if (this.weight < d.weight) return 1;
-                else if (this.weight > d.weight) return -1;
-                else return 0;
+                int chunks = chunkFreq.get(term);
+                float frac = NUMBER_OF_CHUNKS/chunks;
+                weight += (freq*Math.log(frac));
             }
         }
-        private class Lemmatizer {
-            protected StanfordCoreNLP pipeline;
-            private Lemmatizer(){
-                Properties props;
-                props = new Properties();
-                props.put("annotators", "tokenize, ssplit, pos, lemma");
 
-                this.pipeline = new StanfordCoreNLP(props);
-            }
-            public ArrayList<String> lemmatize(String documentText) {
-                ArrayList<String> lemmas = new ArrayList<>();
-                Annotation document = new Annotation(documentText);
+        public int compareTo(DocData d) {
+            if (this.weight < d.weight) return 1;
+            else if (this.weight > d.weight) return -1;
+            else return 0;
+        }
+    }
+    private static class Lemmatizer {
+        protected StanfordCoreNLP pipeline;
+        private Lemmatizer(){
+            Properties props;
+            props = new Properties();
+            props.put("annotators", "tokenize, ssplit, pos, lemma");
+
+            this.pipeline = new StanfordCoreNLP(props);
+        }
+        public ArrayList<String> lemmatize(String documentText) {
+            ArrayList<String> lemmas = new ArrayList<>();
+            Annotation document = new Annotation(documentText);
                 // run all Annotators on this text
-                this.pipeline.annotate(document);
+            this.pipeline.annotate(document);
                 // Iterate over all of the sentences found
-                List<CoreMap> sentences = document.get(SentencesAnnotation.class);
-                for(CoreMap sentence: sentences) {
+            List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+            for(CoreMap sentence: sentences) {
                     // Iterate over all tokens in a sentence
-                    for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+                for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
                         // Retrieve and add the lemma for each word into the
                         // list of lemmas
-                        lemmas.add(token.get(LemmaAnnotation.class));
-                    }
+                    lemmas.add(token.get(LemmaAnnotation.class));
                 }
-                return lemmas;
             }
+            return lemmas;
         }
     }
 }
